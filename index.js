7e58c54fd3365fffaf9fbb7e71a6a44de45bd50c
kth012 <skethiri@gmail.com>
2021-06-24 16:59:31 +0530

    Add file upload form
    Replace http module with express framework
    
    Add formidable module to handle file upload.
    
    Add mv module to move file. rename function from filesystem module does not support renaming files from a different mount, hence mv module is used to move the uploaded file to resource directory.
    
    Add express.Router() to handle routes.

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-var http = require('http');
+const express = require('express');
+const router = express.Router();
+const app = express()
 
-http.createServer(function (req, res) {
-  res.write('Speech To Text Service');
-  res.end();
-}).listen(process.env.PORT || 8080);
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
+const mv = require('mv');
+
+// create resouce folder to save the m4a files in the server
+const resourcepath = path.join(__dirname, 'resources');
+fs.mkdirSync(resourcepath, { recursive: true });
+
+// render the file upload form
+router.get('/',(req,res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+});
+
+// process the fileupload form action
+router.post('/fileupload',(req,res) => {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    var oldpath = files.filetoupload.path;
+    var newpath = path.join(resourcepath, files.filetoupload.name);
+    console.log(`A new file is uploaded and saved in ${newpath}`);
+    mv(oldpath, newpath, function (err) {
+      if (err) next(err);
+      res.write('Your file is uploaded and being processed!');
+      res.end();
+    });
+  });
+});
+
+// redirect to root route in case if invalid route
+router.get('/*',(req,res) => {
+  res.redirect('/');
+})
+
+app.use('/', router);
+app.listen(process.env.PORT || 8080 ,() => {
+  console.log(`Server started on ${process.env.PORT}`);
+})
